46b08c29fa031ca9cc8d39889dcb128b5dad8b7d
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 16:14:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,1 @@
-def my_calculator(first_number, second_number)
-  puts first_number + second_number
-  puts first_number - second_number
-  puts first_number * second_number
-  puts first_number / second_number
-end
+first_number = 5
